cbf5293e4dbd23be12a588e05ad00f56b91c011a
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-07 14:48:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,7 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
- 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
+  now = Time.now
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
